63d2c486148b72d3ff6152ec2e8dda479fcc1ee5
Kayode Adeniyi <adeniyikayode22@gmail.com>
2021-07-20 05:58:55 +0100

    connected to db

--- music/views.py
+++ music/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
+from .models import Album
 
 def index(request):
-    return HttpResponse('THIS IS A TEST')
+    all_albums = Album.objects.all()
+    html = ''
 
+def detail(request, album_id):
+    return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
 
-# Create your views here.
